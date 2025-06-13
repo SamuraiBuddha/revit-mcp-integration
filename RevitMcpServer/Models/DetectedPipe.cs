2a76f88e55f75a3ef94f1951d9761a4639d47bef
Jordan Paul Ehrig <76662139+SamuraiBuddha@users.noreply.github.com>
2025-06-13 18:28:42 -0400

    Update DetectedPipe with typed properties and add MEPSystemType enum

--- RevitMcpServer/Models/DetectedPipe.cs
+++ RevitMcpServer/Models/DetectedPipe.cs
@@ -1,5 +1,6 @@
 using Autodesk.Revit.DB;
 using System.Collections.Generic;
+using RevitMcpServer.Controllers;
 
 namespace RevitMcpServer.Models
 {
@@ -9,9 +10,46 @@ namespace RevitMcpServer.Models
         public XYZ StartPoint { get; set; }
         public XYZ EndPoint { get; set; }
         public double Diameter { get; set; }
-        public string SystemType { get; set; }
+        public MEPSystemType SystemType { get; set; }
+        public PipeMaterial Material { get; set; }
         public double Confidence { get; set; }
         public List<XYZ> CenterlinePoints { get; set; }
+        public CylindricalCenterline Centerline { get; set; }
         public Dictionary<string, object> Properties { get; set; }
+        
+        public DetectedPipe()
+        {
+            CenterlinePoints = new List<XYZ>();
+            Properties = new Dictionary<string, object>();
+            Material = PipeMaterial.Unknown;
+            SystemType = MEPSystemType.Unknown;
+        }
+    }
+    
+    /// <summary>
+    /// MEP system types for scan detection
+    /// </summary>
+    public enum MEPSystemType
+    {
+        Unknown,
+        DomesticColdWater,
+        DomesticHotWater,
+        SupplyHydronic,
+        ReturnHydronic,
+        FireProtectionWet,
+        FireProtectionDry,
+        SanitaryWaste,
+        StormDrainage,
+        VentPiping,
+        Steam,
+        Condensate,
+        NaturalGas,
+        CompressedAir,
+        SupplyAir,
+        ReturnAir,
+        ExhaustAir,
+        PowerElectrical,
+        LightingElectrical,
+        DataCommunication
     }
 }
